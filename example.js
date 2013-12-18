736e05f9b4d5422299243c275a5f72c3cc677887
Chris Lee <chrislee@bom.gov.au>
2013-12-18 11:45:05 +1100

    put the state variables into datatable object

---
+++ example.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+
+	//load up some fake data
+	for (index  = 0; index < 5000; ++index) {
+			
+			// add each row as an object in array
+			row = new Object();
+			row.name = index + 1;
+			row.type = Math.random();
+			row.state = Math.random();
+			dataTable.model[index] = row;					
+
+	}
+	
+
+	// make the HTML table from array model
+	renderTable();
+
+});
--- fast-datatable.js
+++ fast-datatable.js
@@ -4,96 +4,83 @@
 //  `-' `-'  `-'`----'  `-'     `----' `-'  `-' `-' `-'  `-' `-' `-'  `-'`----' `----'`----'`----' 
 
 
-$( document ).ready(function() {
 
-	//state of the table
-	var tableModel = new Array();
-	var previousModel;
-	var plant;
-	var currentPage = 1;
-	var numberToDisplay = 15;
-	var pageCount;
-
-	function previousPage() {
-		if(currentPage - 1 > 0)
-			currentPage --;
+// the state of the datatable is represented by this object
+var dataTable = new Object();
 	
-		renderTable();
-	}
+dataTable.model = new Array();
+dataTable.currentPage = 1;
+dataTable.numberToDisplay = 15;
+dataTable.pageCount = 0;
 
-	function nextPage() {
-		if(currentPage + 1 <= pageCount)
-			currentPage ++;
-			
-		renderTable();
-	}	
+function dataTable() {
 
-	//load up some fake data
-	for (index  = 0; index < 5000; ++index) {
-				
-			// add each row as an object in array
-			row = new Object();
-			row.name = index + 1;
-			row.type = Math.random();
-			row.state = Math.random();
-			tableModel[index] = row;					
+}
 
-	}
+function previousPage() {
+	if(dataTable.currentPage - 1 > 0)
+		dataTable.currentPage --;
 
-	////////////////////////////////////////////////////////////////////
-	//
-	// Using the array make a HTML table
-	//
-	////////////////////////////////////////////////////////////////////
-
-	function renderTable() {
-		var row;
-		var headerRow;
-		var tbody;
-		pageCount = tableModel.length / numberToDisplay;
-		pageCount = Math.ceil(pageCount);
-		var startIndex = (numberToDisplay * currentPage) - numberToDisplay;
-		var endIndex = (numberToDisplay * currentPage) -1;
-		var displayingStart = startIndex + 1;
-		var displayingEnd;
-	
-		// don't let the end index exceed the length of the array
-		if (endIndex + 1 > tableModel.length) {
-			displayingEnd = tableModel.length;
-			endIndex = tableModel.length -1;
-		} else {
-			displayingEnd = endIndex + 1;
-		}
-
-		$("#table_tbody").remove();
-		tbody = $("<tbody></tbody>").attr({ id: "table_tbody" }).appendTo($("#datatable"));
-
-		// update the table synnary
-		$("#table_summary_text").remove();
-		$("<p></p>").attr({ id: "table_summary_text" }).text("Page " + currentPage + " of " + pageCount + ". Displaying " + displayingStart +"-" + displayingEnd + " of " + tableModel.length +" items.").appendTo("#table_summary");
-
-
-		for (index = startIndex; index <= endIndex; ++index) {
-			row = $('<tr></tr>').appendTo(tbody);
-			$('<td></td>').text(tableModel[index].name).appendTo(row); 
-			$('<td></td>').text(tableModel[index].type).appendTo(row); 
-			$('<td></td>').text(tableModel[index].state).appendTo(row); 
-		}
-	
-	}
-
-
-	// make the HTML table from array model
 	renderTable();
-	
-	// buttons
-	// table pagination
-	$("#previous_page").click(function(){
-		previousPage();
-	});	
-	$("#next_page").click(function(){
-		nextPage();
-	});	
+}
+
+function nextPage() {
+	if(dataTable.currentPage + 1 <= dataTable.pageCount)
+		dataTable.currentPage ++;
+		
+	renderTable();
+}	
+
+////////////////////////////////////////////////////////////////////
+//
+// Using the array make a HTML table
+//
+////////////////////////////////////////////////////////////////////
+
+function renderTable() {
+	var row;
+	var headerRow;
+	var tbody;
+	dataTable.pageCount = dataTable.model.length / dataTable.numberToDisplay;
+	dataTable.pageCount = Math.ceil(dataTable.pageCount);
+	var startIndex = (dataTable.numberToDisplay * dataTable.currentPage) - dataTable.numberToDisplay;
+	var endIndex = (dataTable.numberToDisplay * dataTable.currentPage) -1;
+	var displayingStart = startIndex + 1;
+	var displayingEnd;
+
+	// don't let the end index exceed the length of the array
+	if (endIndex + 1 > dataTable.model.length) {
+		displayingEnd = dataTable.model.length;
+		endIndex = dataTable.model.length -1;
+	} else {
+		displayingEnd = endIndex + 1;
+	}
+
+	$("#table_tbody").remove();
+	tbody = $("<tbody></tbody>").attr({ id: "table_tbody" }).appendTo($("#datatable"));
+
+	// update the table synnary
+	$("#table_summary_text").remove();
+	$("<p></p>").attr({ id: "table_summary_text" }).text("Page " + dataTable.currentPage + " of " + dataTable.pageCount + ". Displaying " + displayingStart +"-" + displayingEnd + " of " + dataTable.model.length +" items.").appendTo("#table_summary");
 
 
-});
+	for (index = startIndex; index <= endIndex; ++index) {
+		row = $('<tr></tr>').appendTo(tbody);
+		$('<td></td>').text(dataTable.model[index].name).appendTo(row); 
+		$('<td></td>').text(dataTable.model[index].type).appendTo(row); 
+		$('<td></td>').text(dataTable.model[index].state).appendTo(row); 
+	}
+
+}
+
+// buttons
+// table pagination
+$("#previous_page").click(function(){
+	previousPage();
+});	
+$("#next_page").click(function(){
+	nextPage();
+});	
+
+
+
